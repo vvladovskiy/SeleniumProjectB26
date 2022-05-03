4fb20bc399f07b9e2b720fa6c4ec4ba772226186
Vick Wladowski <vvladovskiy@gmail.com>
2022-05-03 11:24:27 -0700

    My Selenium May 3 11 am

---
+++ src/test/java/com/cydeo/SeleniumTest.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+
+        // 1- Setting up WebDriver manager
+        WebDriverManager.chromedriver().setup();
+
+        // 2- Create instance of Chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
+    }
+}
