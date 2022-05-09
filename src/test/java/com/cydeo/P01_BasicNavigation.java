1df1dfb1d16cde551057cdb946902bee315c9bf8
Vick Wladowski <vvladovskiy@gmail.com>
2022-05-08 21:01:58 -0700

    My Selenium May 7 27 pm

--- src/test/java/com/cydeo/P01_BasicNavigation.java
+++ src/test/java/com/cydeo/P01_BasicNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class P01_BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 //set up chrome and create WebDriver instance
         WebDriverManager.chromedriver().setup();
@@ -14,21 +14,30 @@ public class P01_BasicNavigation {
         // navigate.to(url)
         driver.navigate().to("https://www.google.com");
 
+        Thread.sleep(2000);
+
         // driver.get(url)
-//        driver.get("https://www.google.com");
+        driver.get("https://www.tesla.com");
 
         // navigate.back()
         driver.navigate().back();
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
         // navigate.forward()
         driver.navigate().forward();
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
         // navigate.refresh()
         driver.navigate().refresh();
+
+        Thread.sleep(3000);
+
+        driver.get("https://www.tesla.com");
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
 
         driver.close();
         driver.quit();
