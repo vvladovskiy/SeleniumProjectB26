fc6a1c513984b30f4a34a0c672d028c632d4d9e0
Vick <vvladovskiy@gmail.com>
2022-06-04 20:56:45 -0700

    Test case 03 May 18 9:00pm

--- src/test/java/com/cydeo/day06/Etsy.java
+++ src/test/java/com/cydeo/day06/Etsy.java
@@ -20,38 +20,49 @@ TC: Etsy checkbox and radio button
 
  */
 public class Etsy {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        Dimension dimension = new Dimension(1200,1000);
+        Dimension dimension = new Dimension(1200, 1000);
         driver.manage().window().setSize(dimension);
 
         driver.navigate().to("https://www.etsy.com");
 
         WebElement searchForWoodenSpoon = driver.findElement(By.xpath("//*[@id=\"global-enhancements-search-query\"]"));
-        searchForWoodenSpoon.sendKeys("wooden spoon"+ Keys.ENTER);
+        searchForWoodenSpoon.sendKeys("wooden spoon" + Keys.ENTER);
 
         WebElement allFiltersButton = driver.findElement(By.xpath("//*[@id=\"search-filter-button\"]"));
         allFiltersButton.click();
 
         WebElement freeShipping = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[2]/fieldset/div/div/div[1]/div/label"));
         freeShipping.click();
+
         WebElement onSale = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[2]/fieldset/div/div/div[2]/div/label"));
         onSale.click();
 
+        WebElement underTwentyFive = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[4]/fieldset/div/div/div[2]/label"));
+        underTwentyFive.click();
+
+        WebElement applyButton = driver.findElement(By.xpath("//*[@id=\"search-filters-overlay\"]/div/div/div[2]/button[2]"));
+        applyButton.click();
+
+        Thread.sleep(600);
+
+//        WebElement myDynamicElement = (new WebDriverWait(driver, Duration.ofSeconds(5))).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"content\"]/div/div[1]/div/div[4]/div[5]/div[3]/div[7]/div[2]/div[1]/span/span/span[1]")));
 
 
+
+        WebElement resultCount = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/div[4]/div[5]/div[3]/div[7]/div[2]/div[1]/span/span/span[1]"));
+        System.out.println("resultCount.getText() = " + resultCount.getText());
 //
+//
+//
+//        WebElement myDynamicElement1 = (new WebDriverWait(driver, Duration.ofSeconds(5))).until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
 
-        WebElement myDynamicElement = (new WebDriverWait(driver, Duration.ofSeconds(5))).until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
-
-
-
-
-
-
+        driver.close();
+        driver.quit();
 
 
     }
--- src/test/java/com/cydeo/day06/StaleElement.java
+++ src/test/java/com/cydeo/day06/StaleElement.java
@@ -1,5 +1,9 @@
 package com.cydeo.day06;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /*
 TC #6: StaleElementReferenceException Task
 
@@ -9,7 +13,51 @@ TC #6: StaleElementReferenceException Task
 4. Refresh the page.
 5. Verify it is displayed, again.
 
+There are 2 situations that we can get this exception:
+1- navigation (handle with reassingn(relocate) the element)
+2- when element deleted from both html and page (handle with try catch)
+This practice is about when navigation refresh happens sometime you can receive this exception and we should know how to handle it with relocating
+
+relocating means; you will find element again;
+cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
 
  */
 public class StaleElement {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Dimension dimension = new Dimension(1200, 1000);
+        driver.manage().window().setSize(dimension);
+
+driver.get("https://practice.cydeo.com/abtest");
+
+        WebElement nameCydeo = driver.findElement(By.cssSelector("[href='https://cydeo.com/']"));
+        System.out.println("linkCydeo.isDisplayed() = " + nameCydeo.isDisplayed());
+
+
+        driver.navigate().refresh();
+
+
+
+        try {
+
+            System.out.println("After refreshing page " + nameCydeo.isDisplayed());
+
+        }catch (StaleElementReferenceException e){
+            System.out.println("StaleElementReferenceException is thrown");
+            System.out.println("This means the web element modified");
+        }
+
+        //System.out.println("linkCydeo.isDisplayed() = " + nameCydeo.isDisplayed());
+
+        WebElement end =  driver.findElement(By.xpath("//*[text()='CYDEO']"));
+        System.out.println(end);
+
+
+        driver.close();
+        driver.quit();
+
+
+    }
 }
